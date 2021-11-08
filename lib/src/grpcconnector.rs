7dd629d05a7b9069e79e306072b1f3a02003d0f2
Little Slingshot <little.slingshot@yandex.com>
2021-11-08 16:48:33 +0000

    fixes to make lib/ possible to compile with wasm-pack build and use wasm-bindgen CLI

--- lib/src/grpcconnector.rs
+++ lib/src/grpcconnector.rs
@@ -6,7 +6,8 @@ use crate::proto::compact_formats::{CompactBlock};
 use crate::proto::service::{BlockID, RawTransaction, LightdInfo, SendResponse};
 
 
-#[wasm_bindgen(module = "/wasmbridge.js")]
+// #[wasm_bindgen(module = "/wasmbridge.js")]
+#[wasm_bindgen]
 extern "C" {
   fn getInfo() -> js_sys::Promise;
 
